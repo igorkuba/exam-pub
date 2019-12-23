9862f209cf0070a08db9aaccc4e0f1c369f6f1ca
Igor <igorkuba1@gmail.com>
2019-12-23 11:04:55 +0100

    Pridané štatistiky

--- src/Controller/SummaryController.php
+++ src/Controller/SummaryController.php
@@ -12,6 +12,22 @@ use App\Service\Paginator;
 class SummaryController extends AbstractController
 {
     /**
+     * @Route("/summary/user", name="summary_all_users")
+     */
+    public function summaryAllUsers(CustomerRepository $customerRep, OrderItemRepository $itemRep, Paginator $paginator)
+    {
+        $customers=$customerRep->findby([],['name'=>'asc']);
+        foreach ($customers as $customer)
+        {
+            $customer->items=$itemRep->itemsByCustomer($customer)->getResult();
+            $customer->sum=$itemRep->sumByCustomer($customer);
+        }
+        return $this->render('summary/summaryAllUsers.html.twig', [
+            'customers' => $customers,
+        ]);
+    }
+    
+    /**
      * @Route("/summary/user/{customerId}", name="summary_user")
      */
     public function summaryUser(CustomerRepository $customerRep, OrderItemRepository $itemRep, Paginator $paginator, int $customerId)
@@ -35,6 +51,22 @@ class SummaryController extends AbstractController
         ]);
     }
     
+    /**
+     * @Route("/summary/product", name="summary_all_products")
+     */
+    public function summaryAllProducts(ProductRepository $productRep, OrderItemRepository $itemRep, Paginator $paginator)
+    {
+        $products=$productRep->findBy([],['name'=>'asc']);
+        foreach ($products as $product)
+        {
+            $product->items=$itemRep->itemsByProduct($product)->getResult();
+            $product->sum=$itemRep->sumByProduct($product);
+        }
+        return $this->render('summary/summaryAllProducts.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
     /**
      * @Route("/summary/product/{productId}", name="summary_product")
      */
